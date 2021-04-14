fd6228547bf0c93513c627060d2188678be0e7e3
samuelroe <samuelroe13@gmail.com>
2021-04-14 11:48:37 +0100

    Chapter 3 Exercises

--- TangoWithDjango/urls.py
+++ TangoWithDjango/urls.py
@@ -20,6 +20,7 @@ from django.urls import include
 
 urlpatterns = [
 path('', views.index, name='index'),
+path('about', views.about, name = 'about'),
 path('rango/', include('rango.urls')),
 # The above maps any URLs starting with rango/ to be handled by rango.
 path('admin/', admin.site.urls),
