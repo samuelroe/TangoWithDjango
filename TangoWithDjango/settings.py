bee8ee18525e78734de8ee9cf9127326624396d6
samuelroe <samuelroe13@gmail.com>
2021-05-10 14:49:36 +0100

    Chapter 4 Completed - Except Image Of Cat Not Working Yet

--- TangoWithDjango/settings.py
+++ TangoWithDjango/settings.py
@@ -19,6 +19,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
