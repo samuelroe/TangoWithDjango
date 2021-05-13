822d56b76f9163923b2ac808d68e955faede9a2c
samuelroe <samuelroe13@gmail.com>
2021-05-13 14:53:57 +0100

    Chapter 6 Initial Commit - Index changes

--- rango/tests.py
+++ rango/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Results From Each Test
+# chapter 3 - 5 out of 12 passed (4 errors/3 fails)
+# chapter 4 - 16 out of 19 passed
+# chapter 5 - 10 out of 11 passed
